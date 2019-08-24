25183feeceed7b4785727c7048ee48ed4f74729b
yabetse <yabetse065@gmail.com>
2019-08-25 01:13:58 +0300

    Added a route to the preloader screen

--- src/routes/router.js
+++ src/routes/router.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const router = express.Router();
 
+router.get('/', (request, response) => {
+    response.render('preloader');
+});
+
 router.get('/editor', (request, response) => {
     response.render('editor');
 });
